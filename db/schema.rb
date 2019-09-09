32e5dec91243f93ec6eefcd5092c372a9b6c998a
KristinHannah <kristinhannahk@gmail.com>
2019-09-08 22:32:07 -0400

    made a raven pointer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_001853) do
+ActiveRecord::Schema.define(version: 2019_09_09_014441) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2019_09_04_001853) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "password_digest"
   end
